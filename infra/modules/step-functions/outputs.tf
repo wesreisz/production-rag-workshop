0272ef889946d9e76a8c4e33518fe9d34538a9c5
wesreisz-tw <wesley.reisz@thoughtworks.com>
2026-03-31 06:28:01 -0400

    initial module 1 start

--- infra/modules/step-functions/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "state_machine_arn" {
-  value = aws_sfn_state_machine.pipeline.arn
-}
-
-output "state_machine_name" {
-  value = aws_sfn_state_machine.pipeline.name
-}
--- modules/chunking-module/src/utils/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-def get_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    return logger
