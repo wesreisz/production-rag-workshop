ee6b54f3193fe3de7b18ee178aa2c1517eb7eb5d
wesreisz-tw <wesley.reisz@thoughtworks.com>
2026-04-11 21:51:06 +0000

    Replaced Docker in linux and modified excludes

--- scripts/build-migration-module.sh
+++ scripts/build-migration-module.sh
@@ -7,11 +7,22 @@ MODULE_DIR="${PROJECT_ROOT}/modules/migration-module"
 
 echo "Installing migration module dependencies..."
 
-docker run --rm \
-  --entrypoint /bin/sh \
-  --platform linux/amd64 \
-  -v "${MODULE_DIR}:/module" \
-  public.ecr.aws/lambda/python:3.11 \
-  -c "pip install -r /module/requirements.txt -t /module --upgrade"
+if command -v docker &>/dev/null; then
+  docker run --rm \
+    --entrypoint /bin/sh \
+    --platform linux/amd64 \
+    -v "${MODULE_DIR}:/module" \
+    public.ecr.aws/lambda/python:3.11 \
+    -c "pip install -r /module/requirements.txt -t /module --upgrade"
+else
+  pip install \
+    --platform manylinux2014_x86_64 \
+    --implementation cp \
+    --python-version 3.11 \
+    --only-binary=:all: \
+    --target "${MODULE_DIR}" \
+    --upgrade \
+    -r "${MODULE_DIR}/requirements.txt"
+fi
 
 echo "Migration module dependencies installed."
